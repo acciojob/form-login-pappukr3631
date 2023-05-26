6d73ae83cacb5edaa2d058e60dad2d44efa79a0a
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-05-26 17:52:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let d = document.getElementsByTagName("input");
+	let fname = d[0].value;
+	let lname = d[1].value;
 
+	alert(fname + " " + lname);
 }
